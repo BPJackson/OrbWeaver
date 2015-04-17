a1158a249f4f42ca513fa249c276659b33ec13d4
Ty Hyten <ty.hyten@gmail.com>
2015-04-17 10:00:30 -0600

    add date range for playlist

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,25 @@
 class DashboardController < ApplicationController
 
   def index
+    # creates Spotify playlist upon callback
     if request.env['omniauth.auth']
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    @playlist = RSpotify::Playlist.find('121425006', '7HRUw3xDnKhKt4KFR3K2lH')
-    @tracks = @playlist.tracks
+    @playlist = @spotify_user.create_playlist!('Orbweaver-Playlist')
     @spotify_user.follow(@playlist)
     end
+    # creates list of bands in town artists that go on sale next week
     @bandtown = BandTown.new
-    @events = @bandtown.denver_event_sales.flatten
+    @bands_on_sale_soon = @bandtown.denver_event_sales.flatten.uniq
+
+    # creates array of Spotify information based on the bands_on_sale_soon array
+    @artists_data = @bands_on_sale_soon.map do |artist|
+    RSpotify::Artist.search("#{artist}", limit: 1, market: "US")
+    end
+    # takes array of artists_data, maps through, and returns the top tracks for each artist
+    # @artists_data[10][0].id (an example that is close to what I'm trying to return)
+    @artists_inner_data = @artists_data[0]
+    @artists_inner_data.map do |artist|
+    end
   end
 
 end
