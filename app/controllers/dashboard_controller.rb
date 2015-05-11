fba036c492db7c0c97caed24b30e2aa3b2843be6
Ty Hyten <ty.hyten@gmail.com>
2015-05-11 17:41:12 -0600

    clean up old code and comments, before modifying logic for bands coming to town

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -17,33 +17,6 @@ class DashboardController < ApplicationController
     @all_top_tracks = @artists_data.map do |artist|
       artist[0].top_tracks(:US)
     end
-    #attempt to get into the @all_top_tracks array and return one array of raw tracks, rather than an array of arrays.
-    # @all_top_tracks = @all_top_tracks.delete_if {|elem| elem.flatten.empty? }
-    #
-    # @tracks_to_add = @all_top_tracks.map do |track|
-    #   track
-    # end
-
-    # @all = @tracks_to_add.map do |track|
-    #   track
-    # end
-
-    # @all_artist_tracks = @artists_data.each do |artist|
-    #   artist[0].each do |a|
-    #     a.top_tracks(:US)
-    #   end
-    # end
-
-    # binding.pry
-
-
-    # takes array of artists_data, maps through, and returns the top tracks for each artist
-    # @artists_data[10][0].id (an example that is close to what I'm trying to return)
-    # @artists_inner_data = @artists_data[0]
-    # @artists_inner_data.map do |artist|
-    #   artist
-    # end
-    # # getting the top tracks for each artist in
 
     # creates Spotify playlist upon callback
     if request.env['omniauth.auth']
@@ -53,9 +26,6 @@ class DashboardController < ApplicationController
       @all_top_tracks.each do |track|
         @playlist.add_tracks!(track, position: 0)
       end
-    # @playlist.add_tracks!(@all_top_tracks[0], position: 0)
-      # end
-    # @playlist.add_tracks!(@all_top_tracks.flatten, position: 0)
     end
 
    end
