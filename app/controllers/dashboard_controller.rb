7aeacb81c1a438ec6aa804e6d61275e0dec2c6ba
Ty Hyten <ty.hyten@gmail.com>
2015-05-13 22:56:38 -0600

    use conditionals to set city instance variable

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,17 @@ class DashboardController < ApplicationController
   def index
     if params[:search]
       @city = params[:search]
+    #   @city = "Kansas City, MO"
+      # @city = request.env["omniauth.params"]["search"]
     else
       @city = "Denver,CO"
     end
+
+
+    if request.env['omniauth.auth']
+      @city = request.env["omniauth.params"]["search"]
+    end
+
       @bandtown = BandTown.new
       # creates list of bands in town artists that go on sale next week for denver, as an array
       @events_array = @bandtown.denver_events(@city).flatten.uniq
@@ -26,16 +34,17 @@ class DashboardController < ApplicationController
       @all_top_tracks = @artists_data.map do |artist|
         artist[0].top_tracks(:US)
       end
-
       # creates Spotify playlist upon callback
       if request.env['omniauth.auth']
       @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
       @playlist = @spotify_user.create_playlist!("Orbweaver-Playlist through #{14.days.from_now.strftime("%m/%d")}")
       @spotify_user.follow(@playlist)
+      # binding.pry
         @all_top_tracks.each do |track|
           @playlist.add_tracks!(track, position: 0)
         end
       end
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   get '/auth/spotify/callback', to: 'dashboard#index'
   post '/', to: 'dashboard#index'
+  get '/auth/spotify', as: 'spotify_auth'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
