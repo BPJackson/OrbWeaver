fd89043f85b3a228988a385459339de826f250c5
Ty Hyten <ty.hyten@gmail.com>
2015-05-20 11:45:05 -0600

    add jquery scroll to arrows

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # This changed to test custom error messages with Gaffe.
   config.consider_all_requests_local       = false
-  
+
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
