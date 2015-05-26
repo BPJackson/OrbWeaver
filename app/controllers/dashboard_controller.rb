12eebc2e9e6f2ae6a76074fafca2fff57d47b722
Ty Hyten <ty.hyten@gmail.com>
2015-05-26 16:58:32 -0600

    moved playlist generating logic into omniauth if statement so api not hit each time page loads

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
 
   def index
+    # this authenitcates app, should improve rate limits
+    RSpotify.authenticate(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
     # checks the incomming params and sets the @city instance variable
     if params[:search]
       @city = params[:search]
@@ -15,25 +17,25 @@ class DashboardController < ApplicationController
     # creates an array of Bandsintown events next week for @city
     @events_array = bandtown.usa_events(@city).flatten.uniq
     # creates an array of artist names, generated from the @events_array
-    artist_list = @events_array.map do |event|
-      event["artists"][0]["name"]
-    end
-    # this authenitcates app, should improve rate limits
-    RSpotify.authenticate(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
-
-    # iterates through each artist and returns an array of RSpotify artist objects. Each RSpotify artist is its own array.
-    rspotify_artists_array = artist_list.map do |artist|
-      RSpotify::Artist.search("#{artist}", limit: 1, market: "US")
-    end
-    # deletes empty arrays in the entire array
-    rspotify_artists_array = rspotify_artists_array.delete_if {|elem| elem.flatten.empty? }
-    # all top tracks for all of the RSpotify artists
-      all_top_tracks = rspotify_artists_array.map do |artist|
-      artist[0].top_tracks(:US)
-    end
 
     # checks for callback Omniauth hash
     if request.env['omniauth.auth']
+
+      artist_list = @events_array.map do |event|
+        event["artists"][0]["name"]
+      end
+
+      # iterates through each artist and returns an array of RSpotify artist objects. Each RSpotify artist is its own array.
+      rspotify_artists_array = artist_list.map do |artist|
+        RSpotify::Artist.search("#{artist}", limit: 1, market: "US")
+      end
+      # deletes empty arrays in the entire array
+      rspotify_artists_array = rspotify_artists_array.delete_if {|elem| elem.flatten.empty? }
+      # all top tracks for all of the RSpotify artists
+      all_top_tracks = rspotify_artists_array.map do |artist|
+        artist[0].top_tracks(:US)
+      end
+
       @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
       playlist = @spotify_user.create_playlist!("Orbweaver-Playlist through #{14.days.from_now.strftime("%m/%d")}")
       @spotify_user.follow(playlist)
