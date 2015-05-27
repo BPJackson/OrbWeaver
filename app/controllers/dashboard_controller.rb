2e453960b21f72b7df6bfb8a570c11563151c146
Ty Hyten <ty.hyten@gmail.com>
2015-05-27 17:17:40 -0600

    switch from gem to faraday

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -26,40 +26,64 @@ class DashboardController < ApplicationController
 
       @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
 
-      
+
+      user_token = request.env['omniauth.auth']['credentials']['token']
       # iterates through each artist and returns an array of RSpotify artist objects. Each RSpotify artist is its own array.
       rspotify_artists_array = artist_list.map do |artist|
-        RSpotify::Artist.search("#{artist}", limit: 1, market: "US")
+        SpotifySearch.new().artist_search(artist, user_token)
+        # RSpotify::Artist.search("#{artist}", limit: 1, market: "US")
       end
+
+      rspotify_artists_array.shift
+      # rspotify_artists_array = rspotify_artists_array.delete_if {|elem| elem.flatten.empty? }
+      # rspotify_artists_array.delete_if { |key| key["artist"]["items"] == []}
+      rspotify_artists_array.delete_if {|x| x["artists"]["items"].empty? }
+
+      artist_ids = rspotify_artists_array.map do |artist|
+          artist["artists"]["items"][0]["id"]
+      end
+
+      all_top_tracks = artist_ids.map do |artist|
+        SpotifySearch.new().artist_top_tracks(artist, user_token)
+      end
+
+
+      all_top_tracks.delete_if {|x| x["tracks"].empty? }
+
+      uri_list = all_top_tracks.map do |track|
+        track["tracks"][0]["album"]["uri"]
+      end
+
+      binding.pry
+
+      playlist = SpotifySearch.new().create_spotify_playlist(@spotify_user.id, user_token)
+
+      SpotifySearch.new().add_spotify_track(playlist["id"], @spotify_user.id, user_token, uri_list)
+
+
       # deletes empty arrays in the entire array
-      rspotify_artists_array = rspotify_artists_array.delete_if {|elem| elem.flatten.empty? }
       # all top tracks for all of the RSpotify artists
-      all_top_tracks = rspotify_artists_array.map do |artist|
-        artist[0].top_tracks(:US)
-      end
 
+      # all_top_tracks = track_ids.map do |track|
+      #   SpotifySearch.new().
+      # end
 
-
-
-
-
-
-      playlist = @spotify_user.create_playlist!("Orbweaver-Playlist through #{14.days.from_now.strftime("%m/%d")}")
-      @spotify_user.follow(playlist)
-      # deletes empty arrays
-      all_top_tracks.delete_if {|elem| elem.flatten.empty? }
-
-      # adds tracks, one batch at a time, to the newly created Orbweaver playlist
-
-      all_top_tracks.flatten!
-
-      playlist.add_tracks!(all_top_tracks[0..99])
-      if all_top_tracks.length > 99
-      playlist.add_tracks!(all_top_tracks[100..199])
-      end
-      if all_top_tracks.length > 199
-        playlist.add_tracks!(all_top_tracks[200..299])
-      end
+      #
+      #
+      # # deletes empty arrays
+      # all_top_tracks.delete_if {|elem| elem.flatten.empty? }
+      #
+      # # adds tracks, one batch at a time, to the newly created Orbweaver playlist
+      #
+      # all_top_tracks.flatten!
+      #
+      # playlist.add_tracks!(all_top_tracks[0..99])
+      # if all_top_tracks.length > 99
+      # playlist.add_tracks!(all_top_tracks[100..199])
+      # end
+      # if all_top_tracks.length > 199
+      #   playlist.add_tracks!(all_top_tracks[200..299])
+      # end
     end
 
   end
---
+++ app/models/spotify_search.rb
@@ -0,0 +1,50 @@
+class SpotifySearch
+  def initialize
+    @conn = Faraday.new(:url => 'https://api.spotify.com/v1/') do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def artist_search(artist, token)
+    response = @conn.get do |req|
+      req.url "search?q=#{artist}&limit=1&type=artist"
+      # req.params['app_id'] = ENV["BANDS_ID"]
+      req.headers['Authorization'] = "Bearer #{token}"
+    end
+    JSON.parse(response.body)
+  end
+
+  def artist_top_tracks(artist_id, token)
+    response = @conn.get do |req|
+      req.url "artists/#{artist_id}/top-tracks"
+      req.params['country'] = "US"
+      req.headers['Authorization'] = "Bearer #{token}"
+    end
+    events = JSON.parse(response.body)
+  end
+
+  def create_spotify_playlist(user_id, token)
+    response = @conn.post do |req|
+      req.url "users/#{user_id}/playlists"
+      # req.params['app_id'] = ENV["BANDS_ID"]
+      req.headers['Authorization'] = "Bearer #{token}"
+      req.body = '{ "name": "Orbweaver-Playlist", "public": true }'
+    end
+    events = JSON.parse(response.body)
+  end
+
+  def add_spotify_track(playlist_id, user_id, token, uris)
+    string = uris.join(', ')
+    response = @conn.post do |req|
+      req.url "users/#{user_id}/playlists/#{playlist_id}/tracks"
+      # req.params['app_id'] = ENV["BANDS_ID"]
+      req.headers['Authorization'] = "Bearer #{token}"
+      req.params['uri'] = "#{string}"
+    end
+    events = JSON.parse(response.body)
+  end
+
+
+end
