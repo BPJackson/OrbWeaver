6f0d0e1333d95a6657df6ec8e8e87de040442c27
Ty Hyten <ty.hyten@gmail.com>
2015-05-27 23:00:01 -0600

    change sleep

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -17,7 +17,7 @@ class DashboardController < ApplicationController
 
     # checks for callback Omniauth hash
     if request.env['omniauth.auth']
-      # spotify_connection = SpotifyRequest.new
+      spotify_connection = SpotifyRequest.new
       # instantiates new RSpotify User
       @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
       # converts @events_array to array of names
@@ -28,10 +28,10 @@ class DashboardController < ApplicationController
       user_token = request.env['omniauth.auth']['credentials']['token']
       # feeds artists names into a Spotify artist search, bridging the gap between the Bandsintown and Spotify API's
       spotify_artists_array = artist_list.each_with_index.map do |artist, i|
-        if i % 4 == 0
+        # if i % 4 == 0
           sleep 0.5
-        end
-        SpotifyRequest.new().artist_search(artist, user_token)
+        # end
+        spotify_connection.artist_search(artist, user_token)
       end
 
       # pops off the first index in the spotify_artists_array, which is junk
@@ -44,7 +44,7 @@ class DashboardController < ApplicationController
       end
       # creates a collection of top tracks data for all artists
       all_top_tracks = artist_ids.map do |artist|
-        SpotifyRequest.new().artist_top_tracks(artist, user_token)
+        spotify_connection.artist_top_tracks(artist, user_token)
       end
       # deletes ["tracks"] with empty values
       all_top_tracks.delete_if {|x| x["tracks"].empty? }
@@ -59,11 +59,11 @@ class DashboardController < ApplicationController
       # slices the array into selections of 89 or less, to accomodate the Spotify request limit
       split_arrays = track_uris.each_slice(89).to_a
       # generates a new playlist in @spotify_user's account
-      playlist = SpotifyRequest.new().create_spotify_playlist(@spotify_user.id, user_token)
+      playlist = spotify_connection.create_spotify_playlist(@spotify_user.id, user_token)
       # adds tracks to playlist
 
       split_arrays.each do |track_uri_group|
-        SpotifyRequest.new().add_spotify_track(playlist["id"], @spotify_user.id, user_token, track_uri_group)
+        spotify_connection.add_spotify_track(playlist["id"], @spotify_user.id, user_token, track_uri_group)
       end
     end
 
