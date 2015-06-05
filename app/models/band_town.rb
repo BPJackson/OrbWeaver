f654f78e6aa6e0427c79eaaf0654dc3a2fb09648
Ty Hyten <ty.hyten@gmail.com>
2015-06-05 10:49:38 -0600

    change date logic

--- app/models/band_town.rb
+++ app/models/band_town.rb
@@ -8,10 +8,14 @@ class BandTown
     end
   end
 
+  def upcomming_week
+
+  end
+
   def usa_events(location)
     response = @conn.get do |req|
       # req.url "/events/search?location=#{location}&radius=40&date=#{14.days.from_now.strftime("%Y-%m-%d")}&per_page=25&format=json"
-      req.url "/events/search?location=#{location}&radius=40&date=#{14.days.from_now.strftime("%Y-%m-%d")}&per_page=50&format=json"
+      req.url "/events/search?location=#{location}&radius=40&date=#{DateTime.now.next_week.strftime("%Y-%m-%d")},#{DateTime.now.next_week.next_day(6).strftime("%Y-%m-%d")}&per_page=50&format=json"
       req.params['app_id'] = ENV["BANDS_ID"]
     end
     events = JSON.parse(response.body)
